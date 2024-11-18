abea9e578ae8e9d0a01f3df21fa97a91095a54a3
JinXzAF <arianf2004@gmail.com>
2024-11-18 16:14:10 -0500

    implement input value and unit extraction

--- read_input.c
+++ read_input.c
@@ -1,28 +1,43 @@
 #include "uniconv.h"
 
-void read_input(int argc, char *argv[], char *type,
-                char input[INPUT_MAX_LENGTH],
+void read_input(int argc, char *argv[], char *type, double *input_value,
+                char input_unit[UNITS_MAX_LENGTH],
                 char output_unit[UNITS_MAX_LENGTH]) {
-  if (argc <= 1) {
-    fprintf(stderr, "PLEASE PROVIDE INPUTS");
+  // Check if all inputs have been provided
+  if (argc != 7) {
+    print_usage();
     exit(EXIT_FAILURE);
   }
 
+  // Loops through all options
   for (int i = 1; i < argc; i++) {
     // Type argument
     if (strcmp(argv[i], "-t") == 0) {
       strcpy(type, argv[i + 1]);
+
+      // TODO: Check if unit type is accepted
     }
 
     // Input argument
     else if (strcmp(argv[i], "-i") == 0) {
-      strcpy(input, argv[i + 1]);
+      int result = sscanf(argv[i + 1], "%lf%s", input_value, input_unit);
 
+      // Check if scanned result is valid
+      if (result != 2) {
+        fprintf(stderr, "Invalid input format.\n");
+        exit(EXIT_FAILURE);
+      }
+
+      // TODO Check if input value is valid
+
+      // TODO Check if input unit is valid
     }
 
     // Output unit argument
     else if (strcmp(argv[i], "-o") == 0) {
       strcpy(output_unit, argv[i + 1]);
+
+      // TODO: Check if ouput unit is accepted
     }
 
     i++;
