ae55f88f4fe1ed1011babeae8fcac60160a33492
JinXzAF <arianf2004@gmail.com>
2024-11-18 16:29:51 -0500

    Added more ignores

--- uniconv.c
+++ uniconv.c
@@ -2,12 +2,14 @@
 
 int main(int argc, char *argv[]) {
   char type;                           // value from -t
-  double input_value;                  // value from -i
+  double input_value;                  // numerical value from -i
   char input_unit[UNITS_MAX_LENGTH];   // unit from -i
   char output_unit[UNITS_MAX_LENGTH];  // value from -o
 
-  // Retrieves type, input and output_unit from command
+  // Retrieve unit type, input value, input unit and output unit from args
   read_input(argc, argv, &type, &input_value, input_unit, output_unit);
+
+  //
 
   printf("%c %lf %s %s\n", type, input_value, input_unit, output_unit);
 
