b38b17246a7cc1ce927788e7b35ff911640284e3
JinXzAF <arianf2004@gmail.com>
2024-11-18 16:14:29 -0500

    reformatted

--- message.c
+++ message.c
@@ -3,18 +3,18 @@
 void print_usage() {
   fprintf(stderr, "Usage:\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "convert [-t t|T|d|D|a|A|v|V|m|M] [-i <number><unit>] [-o <unit>] " "[...options]");
+  fprintf(stderr,
+          "convert [-t t|T|d|D|a|A|v|V|m|M] [-i <number><unit>] [-o <unit>] "
+          "[...options]");
 }
 
-void print_help() { 
-  printf("HELP MENU\n"); 
+void print_help() {
+  printf("HELP MENU\n");
   printf("Options:\n");
   printf("  -t TYPE             Specify the type of conversions.\n");
-  printf("                      t/T: Temprature\n");
-  printf("                      d/D: Distance\n");
-  printf("                      a/A: Area\n");
-  printf("                      v/V: Volume\n");
+  printf("                      t|T: Temprature\n");
+  printf("                      d|D: Distance\n");
+  printf("                      a|A: Area\n");
+  printf("                      v|V: Volume\n");
   printf("");
-
-  
-  }
+}
